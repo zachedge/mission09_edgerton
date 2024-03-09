cf0aeba7c35fe6f05cb8a2389c34d81ad146f68b
zachedge <103464298+zachedge@users.noreply.github.com>
2024-03-08 19:37:43 -0700

    this should be finalized

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,63 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+interface TeamProperties {
+  school: string;
+  name: string;
+  mascot: string;
+  city: string;
+  state: string;
+}
+
+const teamData: TeamProperties[] =
+  require('./CollegeBasketballTeams.json').teams;
+
+function Welcome() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>This is March Madness!</h1>
+      <h3>
+        Here you find school names, the names of their mascots, and where the
+        school is found.
+      </h3>
+      <br />
     </div>
   );
 }
 
+class TeamList extends React.Component<TeamProperties> {
+  render() {
+    const { school, name, city, state } = this.props;
+
+    return (
+      <div>
+        <h2>{school}</h2>
+        <h3>Mascot: {name}</h3>
+        <h3>
+          Location: {city}, {state}
+        </h3>
+        <br />
+      </div>
+    );
+  }
+}
+
+function TeamGroup() {
+  return (
+    <div>
+      {teamData.map((team, index) => (
+        <TeamList key={index} {...team} />
+      ))}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App">
+      <Welcome />
+      <TeamGroup />
+    </div>
+  );
+}
 export default App;
